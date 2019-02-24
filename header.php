f18b222a7abca2d8e82e98cf6a71af69b7983f04
Marinos <marinos@crowdpolicy.com>
2019-02-24 16:53:04 +0200

    header

--- header.php
+++ header.php
@@ -22,5 +22,12 @@
 <body <?php body_class(); ?>>
     <div class="container">
         <header class="header">
-
+            <div class=header__text-box>
+                <a href="<?php echo home_url(); ?>" class="header__home-link">
+                    <h1 class="heading-primary">
+                        <span class="heading-primary--main">ΤΕΝΤΕΣ</span>
+                        <span class="heading-primary--sub">ΡΟΜΝΙΟΣ</span>
+                    </h1>
+                </a>
+            </div>
         </header> 
