60ed12b6402fdf4e9ba345cb5fb2186ac2ea25c8
Marinos <marinos@crowdpolicy.com>
2019-03-06 18:42:24 +0200

    improved burger functionality

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,10 @@
 <aside class="sidebar">
-    <div class="burger">
-        <span class="bun"></span>
-        <span class="bun"></span>
-        <span class="bun"></span>
-    </div>
     <div class="sidebar__icon-box">
+        <div class="sidebar__icon burger">
+            <div class="bun"></div>
+            <div class="bun"></div>
+            <div class="bun"></div>
+        </div>
         <div class="sidebar__icon"><i class="fas fa-phone"></i></div>
         <div class="sidebar__icon"><i class="fas fa-envelope"></i></div>
     </div>
