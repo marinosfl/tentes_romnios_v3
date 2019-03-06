8f741b056000873231cd9d2659082a2c8b3fb1ea
Marinos <marinos@crowdpolicy.com>
2019-03-06 10:15:49 +0200

    removed swiper cdns from head

--- header.php
+++ header.php
@@ -15,6 +15,9 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
     <!-- FONT-AWESOME 5.4.2 -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+    <!-- 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/js/swiper.min.js"></script> -->
 
     <?php wp_head(); ?>
 </head>
