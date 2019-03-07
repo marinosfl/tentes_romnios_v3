4dcbcb0486c104455a931085ac0d4bd50ed22943
Marinos <marinos@crowdpolicy.com>
2019-03-07 13:29:46 +0200

    desktop sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,6 @@
 <aside class="sidebar">
+    <?php include('header_text-box.php'); ?>
+    <hr />
     <div class="sidebar__icon-box">
         <div class="sidebar__icon burger">
             <div class="bun"></div>
@@ -10,5 +12,21 @@
     </div>
     <nav class="navbar" role="navigation">
         <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
+        <hr />
+        <div class="contact">
+            <div class="contact--info contact--phone">
+                <i class="fas fa-phone"></i>
+                <h3>210 933 0676</h3>
+            </div>
+            <div class="contact--info contact--mobile">
+                <i class="fas fa-mobile-alt"></i>
+                <h3>694 851 2600</h3>
+            </div>
+            <div class="contact--info contact--email">
+                <i class="fas fa-envelope"></i>
+                <h3>romnios@hotmail.com</h3>
+            </div>
+        </div>
     </nav>
+
 </aside> 
