e397828161c037f022409064be2ecde64fc69207
Marinos <marinos@crowdpolicy.com>
2019-03-14 21:10:42 +0200

    hiding mobile menu on item click

--- src/js/views/productView.js
+++ src/js/views/productView.js
@@ -4,17 +4,26 @@ const { root, product, firstProduct, secondProduct, thirdProduct } = elements;
 
 export const checkProduct = target => {
   const productHeight = product.height();
+  // console.log(target);
+  // console.log(secondProduct);
 
   if (target[0] == firstProduct[0]) {
     root.style.setProperty('--product-active', 0);
     setMaxHeight('first');
   } else if (target[0] == secondProduct[0]) {
+    console.log('asda');
     const height = '-' + productHeight + 'px';
     root.style.setProperty('--product-active', height);
     setMaxHeight('second');
   } else if (target[0] == thirdProduct[0]) {
     const height = '-' + productHeight * 2 + 'px';
     root.style.setProperty('--product-active', height);
+  }
+
+  if (checkWindowWidth() < 1024) {
+    console.log(checkWindowWidth);
+    elements.navbar.removeClass('active');
+    elements.sidebarIcons.removeClass('active');
   }
 };
 
