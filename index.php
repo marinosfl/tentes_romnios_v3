bdd7c84dab12f5a411e76920fb022cb43c08eb4b
Marinos <marinos@crowdpolicy.com>
2019-03-17 16:04:17 +0200

    added  class on product_kataskeues, minus comments

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
                 <h2 class="product__title">ΤΕΝΤΕΣ</h2>
                 <div class="product__description">
                     <h2 class="item-heading">
-                        <!-- <i class="fas fa-angle-left"></i> -->
                         <i class="fas fa-angle-down"></i>
                         <span>ΤΕΝΤΕΣ</span>
                     </h2>
@@ -17,21 +16,7 @@
                 </div>
             </div>
             <div class="product__categories">
-                <!-- <a href="https://cpdev.crowdpolicy.com/marinos_tentes/tentes_tax/antirida/" class="product__category">
-                    <h2 class="product__category--title">ΑΝΤΙΡΙΔΑ</h2>
-                </a>
-                <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΚΑΘΕΤΗ</h2>
-                </a>
-                <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΒΡΑΧΙΟΝΑ</h2>
-                </a>
-                <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΚΑΣΕΤΙΝΑ</h2>
-                </a>
-                <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΚΟΥΡΜΠΕΣ</h2>
-                </a> -->
+
             </div>
         </div>
         <div class="product product__pergoles" data-taxonomy="pergoles_tax">
@@ -40,7 +25,6 @@
                 <h2 class="product__title">ΠΕΡΓΚΟΛΕΣ</h2>
                 <div class="product__description">
                     <h2 class="item-heading">
-                        <!-- <i class="fas fa-angle-left"></i> -->
                         <i class="fas fa-angle-down"></i>
                         <span>ΠΕΡΚΟΛΕΣ</span>
                     </h2>
@@ -51,30 +35,15 @@
 
             </div>
             <div class="product__categories">
-                <!-- <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΑΝΤΙΡΙΔΑ</h2>
-                </a>
-                <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΚΑΘΕΤΗ</h2>
 
-                </a>
-                <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΒΡΑΧΙΟΝΑ</h2>
-
-                </a>
-
-                <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΚΟΥΡΜΠΕΣ</h2>
-                </a> -->
             </div>
         </div>
-        <div class="product">
+        <div class="product product_kataskeues">
             <div class="product__top-img">
                 <div class="product__top-img--hover"></div>
                 <h2 class="product__title">ΕΙΔΙΚΕΣ ΚΑΤΑΣΚΕΥΕΣ</h2>
                 <div class="product__description">
                     <h2 class="item-heading">
-                        <!-- <i class="fas fa-angle-left"></i> -->
                         <i class="fas fa-angle-down"></i>
                         <span>ΕΙΔΙΚΕΣ ΚΑΤΑΣΚΕΥΕΣ</span>
                     </h2>
@@ -84,23 +53,7 @@
                 </div>
             </div>
             <div class="product__categories">
-                <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΑΝΤΙΡΙΔΑ</h2>
-                </a>
-                <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΚΑΘΕΤΗ</h2>
 
-                </a>
-                <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΒΡΑΧΙΟΝΑ</h2>
-
-                </a>
-                <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΚΑΣΕΤΙΝΑ</h2>
-                </a>
-                <a href="#!" class="product__category">
-                    <h2 class="product__category--title">ΚΟΥΡΜΠΕΣ</h2>
-                </a>
             </div>
         </div>
     </main>
